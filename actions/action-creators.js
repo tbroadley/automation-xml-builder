21c480170ffd6444ad438fbe113fd5b2a479e1bc
Thomas Broadley <buriedunderbooks@hotmail.com>
2015-10-19 14:57:43 +0900

    added makeActionCreator to reduce action creator boilerplate

--- actions/action-creators.js
+++ actions/action-creators.js
@@ -5,31 +5,18 @@ import {
   SET_JOB_SCHEDULE_ONE_TIME_DATE,
 } from './actions';
 
-export function setJobName(name) {
-  return {
-    type: SET_JOB_NAME,
-    name,
+function makeActionCreator(type, ...argNames) {
+  return function(...args) {
+    let action = { type };
+    argNames.forEach((arg, index) => {
+      action[argNames[index]] = args[index];
+    });
+    return action;
   };
 }
 
-export function setJobDescription(description) {
-  return {
-    type: SET_JOB_DESCRIPTION,
-    description,
-  };
-}
+export const setJobName = makeActionCreator(SET_JOB_NAME, 'name');
+export const setJobDescription = makeActionCreator(SET_JOB_DESCRIPTION, 'description');
 
-
-export function setJobScheduleType(scheduleType) {
-  return {
-    type: SET_JOB_SCHEDULE_TYPE,
-    scheduleType,
-  };
-}
-
-export function setJobScheduleOneTimeDate(scheduleOneTimeDate) {
-  return {
-    type: SET_JOB_SCHEDULE_ONE_TIME_DATE,
-    scheduleOneTimeDate,
-  };
-}
+export const setJobScheduleType = makeActionCreator(SET_JOB_SCHEDULE_TYPE, 'description');
+export const setJobScheduleOneTimeDate = makeActionCreator(SET_JOB_SCHEDULE_ONE_TIME_DATE, 'description');
