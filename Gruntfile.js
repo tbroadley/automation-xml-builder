5dcd633fb7dd5607ee322009d43d53a4acf3fc8c
Thomas Broadley <buriedunderbooks@hotmail.com>
2015-10-22 21:32:30 +0900

    use Grunt to build the project

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-execute');
+  grunt.loadNpmTasks('grunt-parallel');
+
+  grunt.registerTask('dev', ['parallel:dev']);
+  grunt.registerTask('prod', ['browserify:prod', 'uglify']);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      dev: {
+        src: 'index.dev.js',
+        dest: 'build/bundle.js',
+        options: {
+          transform: [['babelify', { optional: ['es7.decorators'] }]],
+          watch: true,
+          keepAlive: true,
+        },
+      },
+      prod: {
+        src: 'index.js',
+        dest: 'build/bundle.js',
+        options: {
+          transform: [['babelify', { optional: ['es7.decorators'] }],
+            'envify', ['uglifyify', { global: true }]]
+        },
+      },
+    },
+    uglify: {
+      build: {
+        src: 'build/bundle.js',
+        dest: 'build/bundle.js',
+      },
+      options: {
+        mangle: true,
+        compress: true,
+      },
+    },
+    execute: {
+        dev: {
+            src: ['server.js'],
+        },
+    },
+    parallel: {
+      dev: {
+        options: {
+          grunt: true,
+          stream: true,
+        },
+        tasks: ['execute:dev', 'browserify:dev'],
+      },
+    },
+  });
+};
--- server.js
+++ server.js
@@ -1,53 +1,8 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-var browserify = require("browserify");
-var watchify = require("watchify");
-var babelify = require("babelify");
-
-var fs = require("fs");
-
 // The port on which the app will listen.
 const PORT = 8080;
-
-// Set up the watchify build.
-var b = browserify({ cache: {}, packageCache: {} });
-b.transform(babelify.configure({
-  optional: ['es7.decorators']
-}));
-
-// Check if the build should be done in development mode.
-// The development build includes redux-devtools.
-var devMode = process.argv.indexOf('dev') !== -1;
-
-if (devMode) {
-  b.add('index.dev.js');
-  console.log('Building in development mode.')
-  console.log('The development build includes redux-devtools and react-addons-perf.')
-} else {
-  b.add('index.js');
-  b.transform('envify');
-  b.transform({ global: true }, 'uglifyify');
-}
-
-var w = watchify(b);
-
-w.on('update', function() {
-  w.bundle()
-    .on('error', function(err) {
-      console.log(err.message);
-    })
-    .pipe(fs.createWriteStream('build/bundle.js'));
-});
-
-w.on('log', function(msg) {
-  console.log(msg);
-});
-
-// Watchify will not emit 'update' events until you've called
-// w.bundle() once and completely drained the stream it returns.
-// An 'update' event is emitted to achieve this.
-w.emit('update');
 
 // Start the web server, serving static files from the current directory.
 connect().use(serveStatic(".")).listen(PORT);
