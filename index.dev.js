2620385ad76fe5080b636510edea094630aa5192
Thomas Broadley <buriedunderbooks@hotmail.com>
2015-10-26 12:21:14 +0900

    included ES5 and String.prototype.repeat polyfills

--- index.dev.js
+++ index.dev.js
@@ -11,6 +11,10 @@ import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import root from './reducers/root';
 import App from './components/app';
 
+import 'es5-shim/es5-shim';
+import 'es5-shim/es5-sham';
+import 'string.prototype.repeat';
+
 const finalCreateStore = compose(
   // Provides support for DevTools:
   devTools(),
--- index.js
+++ index.js
@@ -7,6 +7,10 @@ import { Provider } from 'react-redux';
 import root from './reducers/root';
 import App from './components/app';
 
+import 'es5-shim/es5-shim';
+import 'es5-shim/es5-sham';
+import 'string.prototype.repeat';
+
 render(
   <Provider store={createStore(root)}>
     <App />
